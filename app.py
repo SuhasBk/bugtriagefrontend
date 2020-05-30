749a560f81bfd9416297d291d93ddfcb56a1dc98
Suhas Kowligi <kowligi1998@gmail.com>
2020-05-30 22:44:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import sys
 import  numpy as np
 import pickle
+from collections import defaultdict
 
 # Flask
 from flask import Flask, redirect, url_for, request, render_template, Response, jsonify, redirect
@@ -15,14 +16,33 @@ print('Model loaded. Check http://127.0.0.1:5000/')
 
 
 def model_predict(bug_id):
-
+    values = defualtdict()
+    
     infile=open('models/pred','rb')
     x=pickle.load(infile)
     print(x)
     yp=np.argmax(x,axis=1)
     outfile=open('models/cl','rb')
     y=pickle.load(outfile)
+    
+    jsonFile = open('models/deep_data.json','rb')
+    temp = json.load(jsonFile)
     print(y)
+    
+    for entry in temp:
+        values[entry[0]] = {
+            'issue_id' : entry[1],
+            'issue_title' : entry[2],
+            'reported_time' : entry[3],
+            'owner' : entry[4],
+            'description' : entry[5]
+        }
+      
+    #for key in values.keys():
+    #    print(values[key]['description'])
+    
+    print(values[bug_id]['description'])
+        
     return y[yp[int(bug_id)]]
 
 
